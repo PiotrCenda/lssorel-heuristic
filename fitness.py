c4e0684e5e2a12fc86515c643b1bea600f1351d2
Szymon <s.mazurek@sanoscience.org>
2023-02-07 14:36:47 +0100

    Final implementations

--- fitness.py
+++ fitness.py
@@ -1,17 +1,44 @@
 import timeit
 import numpy as np
-import wandb
+#import wandb
+import xlastovka
 
+def on_generation_xlastovka(ga_instance):
+    """Determine what happens with the beginning of every generation."""
+    print(f"Generation = {ga_instance.generations_completed}")
+    print(f"Best solution fitness pre local= {ga_instance.best_solutions_fitness[-1]}")
+
+    for n,old_solution in enumerate(ga_instance.population):
+        old_energy = ga_instance.last_generation_fitness[n]
+        new_solution, new_fitness = xlastovka.xlastovka_search(old_solution, len(old_solution), old_energy,max_iter=1000)
+        if new_fitness > old_energy:
+            ga_instance.population[n] = new_solution
+            ga_instance.last_generation_fitness[n] = new_fitness
+        best_solution, best_solution_fitness, best_match_idx = ga_instance.best_solution(
+            pop_fitness=ga_instance.last_generation_fitness
+            )
+        ga_instance.best_solutions_fitness[-1] = best_solution_fitness
+    print(f"Best solution fitness post local= {ga_instance.best_solutions_fitness[-1]}")
+    # wandb.log(
+    #     {
+    #         "fitness": ga_instance.best_solutions_fitness[-1],
+    #         "generation": ga_instance.generations_completed,
+    #     }
+    # )
 def on_generation(ga_instance):
     """Determine what happens with the beginning of every generation."""
     print(f"Generation = {ga_instance.generations_completed}")
-    print(f"Best solution fitness = {ga_instance.best_solutions_fitness[-1]}")
-    wandb.log(
-        {
-            "fitness": ga_instance.best_solutions_fitness[-1],
-            "generation": ga_instance.generations_completed,
-        }
-    )
+    print(f"Best solution fitness pre local= {ga_instance.best_solutions_fitness[-1]}")
+    # wandb.log(
+    #     {
+    #         "fitness": ga_instance.best_solutions_fitness[-1],
+    #         "generation": ga_instance.generations_completed,
+    #     }
+    # )
+
+
+
+
 
     
 
@@ -22,24 +49,13 @@ def fitness_func_energy(solution, sol_idx=None):
         solution: Numpy array consisting of -1 and 1. 
         sol_idx: Variable is requested by pygad
     """
-    # off-peak autocorrelations
-
-    autocorrelations = np.correlate(solution, solution, 'full')
-    autocorrelations = autocorrelations[-len(solution)+1:]
-    # the energy
-    solution_fitness = np.sum(np.power(autocorrelations, 2))
-    ## I suppose we only need to minimize energy 
-    return -solution_fitness
+    return xlastovka.calculate_energy(solution)
 
 
 if __name__ == "__main__":
-    random_solution_sequence = np.random.choice([-1, 1], size=10000, replace=True)
-    print(f"Sequence: {random_solution_sequence}\n")
-    
+    random_solution_sequence = np.random.choice([-1, 1], size=64, replace=True)
     t = timeit.Timer(lambda: fitness_func_energy(random_solution_sequence)) 
-    autocorrelation, energy = fitness_func_energy(random_solution_sequence)
-    
-    print(f"Autocorrelation: {autocorrelation}")
+    energy = fitness_func_energy(random_solution_sequence)
     print(f"Energy: {energy}")
     print(f"Runtime: {t.timeit(30)}s\n")
     
--- xlastovka.py
+++ xlastovka.py
@@ -1,5 +1,10 @@
 from typing import Tuple, List
-from fitness import fitness_func_energy
+import numpy as np
+def calculate_energy(sequence):
+    autocorrelations = np.correlate(sequence, sequence, 'full')
+    autocorrelations = autocorrelations[-len(sequence)+1:]
+    energy =  np.sum(np.power(autocorrelations, 2))
+    return -energy
 
 
 def flip_bit(coord: List[int], i: int) -> List[int]:
@@ -12,18 +17,20 @@ def flip_bit(coord: List[int], i: int) -> List[int]:
     Returns:
         List[int]: flipped sequence
     """
-    
-    coord[i] = 1 - coord[i]
-    
+  
+    coord[i] = - coord[i]
+
     return coord
 
 
-def xlastovka_search(L: int, valueTarget: int) -> Tuple[int, int]:
+def xlastovka_search(sequence : List[int], L: int, valueTarget: int, max_iter : int = 1000) -> Tuple[int, int]:
     """Performs xLastovka local search algorithm.
 
     Args:
+        sequence (List[int]): initial sequence of bits
         L (int): instance size
         valueTarget (int): best upper bound
+        max_iter (int, optional): maximum number of iterations for local search. Defaults to 1000.
 
     Returns:
         Tuple[int, int]: (coord) best coordinate and best value
@@ -33,40 +40,61 @@ def xlastovka_search(L: int, valueTarget: int) -> Tuple[int, int]:
     PQ = list()
 
     # storage of pivots
-    closePivots = set()
+    closePivots = []
 
     # initialize coordinate
-    coord = RandomSolution(L)  # TODO
+    coord = list(sequence)  # TODO
     
     # evaluation
-    value =  fitness_func_energy(coord)
-    
-    # stopping criteria
-    if value <= valueTarget:  # TODO: check sign and whole maximize/minimize thing 
-        return coord, value 
-    
+    value =  valueTarget
+
     PQ.append((coord, value))
+
+    n_iter = 0
+    while n_iter < max_iter:
+        n_iter += 1
     
-    while value > valueTarget:
-        coord, _ = PQ.pop(0)
-        
+        if len(PQ) == 0:
+            break ## if there are no more unique pairs to check break the loop
+        else:
+            coord, _ = PQ.pop(0)
+    
         # search neighborhood
-        for i in range(0, (L + 1) / 2 + 1):
+        for i in range(0, int(L/ 2) + 1): ## removed L+1 as it was float
             # flip i-th bit
             sFlipped = flip_bit(coord, i)
-            
             # skip if already pivot
+            
             if sFlipped in closePivots:
+                
                 continue
             else:
-                value = fitness_func_energy(sFlipped)
-            
+                value = calculate_energy(sFlipped)
+               
             # stopping criteria
-            if value <= valueTarget:  # TODO: again, check sign and whole maximize/minimize thing
-                break
+            if value > valueTarget:
+                return sFlipped, value
+                # stop_flag = True  ## if we found better value,break the loop and return the 
+                
+                # break
             else:
+                coord = sFlipped
                 PQ.append((sFlipped, value))
             
-        closePivots.add(coord)
-    
-    return coord, value
+        closePivots.append(coord)
+    # if value < valueTarget:
+    #     ## if we failed to find new best value, return the old one with old coords
+    #     best_value = valueTarget
+    #     coord = sequence
+    # else:
+    #     best_value = value
+    return coord, valueTarget
+if __name__ == "__main__":
+    for _ in range(1000):
+        random_solution_sequence = np.random.choice([-1, 1], size=16, replace=True)
+        energy_init = calculate_energy(random_solution_sequence)
+        coord,energy = xlastovka_search(random_solution_sequence,len(random_solution_sequence), energy_init)
+        if energy_init > energy:
+            print(f"Something went wrong! New fitness value is {energy}, old is {energy_init}")
+    print(f"New fitness value is {energy}, old is {energy_init}")
+
